047c8561bb47caac7054687cebf289afca634592
Heráclito Pantoja Junior <juniorpantoja2301@gmail.com>
2022-05-10 15:42:14 -0300

    Adiciona validation banco

--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php
 
-$temEmail = true;
-$emailValido = true;
 
-if($temEmail = true && $temEmail = true) {
-    echo "Enviado";
-} elseif ($temEmail = false ) {
+$arquivoValido  = false;
+$emailValido = false;
 
+#1 válido nos dois
+if ($arquivoValido == true and $emailValido == true ) {
+    echo " Envidado </ br > " ;
 }
 
-var_dump ($temEmail, $emailValido);
+#2 válido somente temEmail
+if ($arquivoValido == true and $emailValido != true) {
+    echo " Email informado não é válido </ br > " ;
+}
+#3 válido somente emailValido
+if ($arquivoValido != true and $emailValido == true) {
+    echo " Arquivo selecionado não é válido </ br > ";
+}
+#4 os dois dados estão inválidos
+if ($arquivoValido == false and $emailValido == false) {
+    echo " Os dois arquivos informados estão inválidos </ br > ";
+}
+var_dump ($arquivoValido, $emailValido);
 
