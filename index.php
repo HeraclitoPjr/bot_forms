d25b8fd3794979d9df5ed117bae9a179f4414ae0
Heráclito Pantoja Junior <juniorpantoja2301@gmail.com>
2022-05-10 10:20:46 -0300

    inicia projeto

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+$temEmail = true;
+$emailValido = true;
+
+if($temEmail = true && $temEmail = true) {
+    echo "Enviado";
+} elseif ($temEmail = false ) {
+
+}
+
+var_dump ($temEmail, $emailValido);
+
